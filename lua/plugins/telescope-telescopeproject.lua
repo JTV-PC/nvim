9aac656d3bac5852ca949a06715158e1abbd8dfb
JTV <jampanatanvai@gmail.com>
2025-11-20 14:34:09 +0530

    Update telescope-telescopeproject.lua and added vim bindings

--- lua/plugins/telescope-telescopeproject.lua
+++ lua/plugins/telescope-telescopeproject.lua
@@ -1,59 +1,73 @@
 -- Telescope + UI-Select + Project integration
 
 return {
-  ---------------------------------------------------------------------------
-  -- TELESCOPE CORE
-  ---------------------------------------------------------------------------
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-ui-select.nvim",
-      "nvim-telescope/telescope-project.nvim",
-    },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+			"nvim-telescope/telescope-project.nvim",
+		},
 
-    config = function()
-      local telescope = require("telescope")
-      local builtin = require("telescope.builtin")
+		config = function()
+			local telescope = require("telescope")
+			local builtin = require("telescope.builtin")
+			local actions = require("telescope.actions")
 
-      telescope.setup({
-        extensions = {
+			telescope.setup({
+				defaults = {
+					initial_mode = "normal",
 
-          -- UI-Select dropdown menu
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({})
-          },
+					mappings = {
+						n = {
+							["h"] = actions.move_selection_previous,
+							["l"] = actions.move_selection_next,
+							["k"] = actions.preview_scrolling_up,
+							["j"] = actions.preview_scrolling_down,
 
-          -- Project management
-          project = {
-            base_dirs = {
-              -- Add your common project folders here if you want:
-              -- { "~/Projects", max_depth = 3 },
-            },
-            hidden_files = true, -- show .git, etc
-            theme = "dropdown",
-            order_by = "recent",
-            sync_with_nvim_tree = true,
-          },
-        },
-      })
+							-- FIXED: manually enter insert mode
+							["i"] = function()
+								vim.cmd("startinsert")
+							end,
 
-      -- Load extensions
-      telescope.load_extension("ui-select")
-      telescope.load_extension("project")
+							["q"] = actions.close,
+						},
 
-      -----------------------------------------------------------------------
-      -- KEYMAPS
-      -----------------------------------------------------------------------
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+						i = {
+							["<C-h>"] = actions.move_selection_previous,
+							["<C-l>"] = actions.move_selection_next,
+							["<C-k>"] = actions.preview_scrolling_up,
+							["<C-j>"] = actions.preview_scrolling_down,
+							["<Esc>"] = actions.close,
+						},
+					},
+				},
 
-      -- Project picker
-      vim.keymap.set("n", "<leader>fp", telescope.extensions.project.project, {
-        desc = "Switch project",
-      })
-    end,
-  },
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+
+					project = {
+						hidden_files = true,
+						theme = "dropdown",
+						order_by = "recent",
+						sync_with_nvim_tree = true,
+					},
+				},
+			})
+
+			telescope.load_extension("ui-select")
+			telescope.load_extension("project")
+
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+
+			vim.keymap.set("n", "<leader>fp", telescope.extensions.project.project, {
+				desc = "Switch project",
+			})
+		end,
+	},
 }
